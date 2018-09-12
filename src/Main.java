3070e4b082c12147924048951f327fd2f59032f7
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-12 22:04:26 +0200

    pierwszy program

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Witaj");
+        int x=50;
+        int y=13;
+        int wynik1, wynik2, wynik3,wynik4;
+        wynik1=x+y;
+        wynik2=x-y;
+        wynik3=x*y;
+        wynik4=x/y;
+
+        System.out.println("Wynik dodawania jest rowny ="+wynik1);
+        System.out.println("Wynik odejmowania jest rowny ="+wynik2);
+        System.out.println("Wynik mnozenia jest rowny ="+wynik3);
+        System.out.print("Wynik dzielenia jest rowny ="+wynik4);
+        System.out.println(" oczywiscie wynik podany bez watrrosci po przecinku :)");
+
+    }
+}
